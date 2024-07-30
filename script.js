ebb2357c8dd966cb01dbc1fe7aae2892abff0687
Anish9345 <146621945+Anish9345@users.noreply.github.com>
2024-07-30 21:45:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function playMusic() {
     const audio = document.getElementById('birthdaySong');
-    audio.play();
+    audio.play().catch(error => {
+        console.log('Autoplay was prevented:', error);
+    });
 }
+
+window.addEventListener('load', playMusic);
